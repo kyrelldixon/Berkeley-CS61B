41ed5581e44de4a9345b4638e746030c06b211e7
Kyrell Dixon <kyrell.brown@gmail.com>
2018-12-18 11:38:49 -0600

    Added Planet file with constructors

---
+++ Projects/proj0/Planet.java
@@ -0,0 +1,31 @@
+/**
+ * Planet
+ */
+public class Planet {
+
+  double xxPos;
+  double yyPos;
+  double xxVel;
+  double yyVel;
+  double mass;
+  String imgFileName;
+
+  public Planet(double xP, double yP, double xV,
+              double yV, double m, String img) {
+    this.xxPos = xP;
+    this.yyPos = yP;
+    this.xxVel = xV;
+    this.yyVel = yV;
+    this.mass = m;
+    this.imgFileName = img;
+  }
+
+  public Planet(Planet p) {
+    this.xxPos = p.xxPos;
+    this.yyPos = p.yyPos;
+    this.xxVel = p.xxVel;
+    this.yyVel = p.yyVel;
+    this.mass = p.mass;
+    this.imgFileName = p.imgFileName;
+  }
+}
