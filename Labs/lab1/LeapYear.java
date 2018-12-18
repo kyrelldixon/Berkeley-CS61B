66cca4072a5711262eec487a2812df52de21fbb9
Kyrell Dixon <kyrell.brown@gmail.com>
2018-12-18 11:12:59 -0600

    Added isLeapYear function with my name

--- Labs/lab1/LeapYear.java
+++ Labs/lab1/LeapYear.java
@@ -1,7 +1,22 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Kyrell Dixon
  */
 public class LeapYear {
+
+    /**
+     * Checks if a given year is a leap year
+     * @param year to be checked
+     */
+    public static boolean isLeapYear(int year) {
+        /**
+         * A leap year is either: divisible by 400 or divisible by 4 and not by 100.
+         */
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
